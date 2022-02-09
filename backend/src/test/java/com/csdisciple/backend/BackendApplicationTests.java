ee8077427835e133c03b09646eea831a6c9369bf
Oleks <{ID}+{username}@users.noreply.github.com>
2022-02-08 22:23:41 -0500

    Initial app structure

---
+++ backend/src/test/java/com/csdisciple/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdisciple.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
